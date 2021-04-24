374a640c79118e3873d1b0447af70bccabfcb1e4
Kriptonorm <77245736+Kriptonorm@users.noreply.github.com>
2021-04-24 22:19:12 +0300

    Update TRC20.sol

--- contracts/TRC20.sol
+++ contracts/TRC20.sol
@@ -5,7 +5,7 @@ contract TRC20 {
     // Public variables of the token
     string public name;
     string public symbol;
-    uint8 public decimals = 8;
+    uint8 public decimals = 18;
     // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
 
@@ -27,9 +27,9 @@ contract TRC20 {
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
-    uint256 initialSupply = 1000000;
-    string tokenName = 'CodeXpertToken';
-    string tokenSymbol = 'CDX';
+    uint256 initialSupply = 500000000;
+    string tokenName = 'XenonToken';
+    string tokenSymbol = 'XNT';
     constructor() public {
 
         totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
